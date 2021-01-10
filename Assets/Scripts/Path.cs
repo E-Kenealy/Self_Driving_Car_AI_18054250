6aaf7b727acfb412936519fb84032426dd3985ff
TheEllisKen <ellis.kenealy@gmail.com>
2021-01-10 15:55:03 +0000

    Initial Push of Progress
    Pushing what has been done so far

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+
+    private List<Transform> nodeList = new List<Transform>();
+
+    void OnDrawGizmosSelected()
+    {
+        if (!Application.isPlaying) return;
+
+        Gizmos.color = Color.magenta;
+
+        Transform[] pathTransforms = GetComponentsInChildren<Transform>();
+        nodeList = new List<Transform>();
+
+        for (int x = 0; x < pathTransforms.Length; x++)
+        {
+            if (pathTransforms[x] != transform)
+            {
+                nodeList.Add(pathTransforms[x]); 
+            }
+        }
+
+        for (int y = 0; y < nodeList.Count; y++)
+        {
+            Vector3 currentNode = nodeList[y].position;
+            Vector3 prevNode = Vector3.zero;
+
+            if (y > 0)
+            {
+                prevNode = nodeList[y - 1].position;
+            }
+            else if (y == 0 && nodeList.Count > 1)
+            {
+                prevNode = nodeList[nodeList.Count - 1].position; //Prevents the counter going to -1 and goes back to end node
+            }
+
+            Gizmos.DrawLine(prevNode, currentNode);
+            Gizmos.DrawWireSphere(currentNode, 0.2f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Power.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    public Transform path;
+    public float maxSteeringAngle = 45f;
+    public WheelCollider frontLeftWheel;
+    public WheelCollider frontRightWheel;
+
+    private List<Transform> nodeList;
+    private int currentNode = 0;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
+        nodeList = new List<Transform>();
+
+        for (int x = 0; x < pathTransforms.Length; x++)
+        {
+            if (pathTransforms[x] != path.transform)
+            {
+                nodeList.Add(pathTransforms[x]);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Steering();
+        Drive();
+    }
+    private void Steering()
+    {
+        Vector3 relativeVector = transform.InverseTransformPoint(nodeList[currentNode].position);
+        float newSteerAngle = (relativeVector.x / relativeVector.magnitude) * maxSteeringAngle;
+        frontLeftWheel.steerAngle = newSteerAngle;
+        frontRightWheel.steerAngle = newSteerAngle;
+    }
+
+    private void Drive()
+    {
+        frontLeftWheel.motorTorque = 3000f;
+        frontRightWheel.motorTorque = 3000f;
+    }
+}
